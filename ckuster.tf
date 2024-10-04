7ede77f8a503b58c3b9e6b80bb62d97a4471e710
shashank <shashanksiddappa@gmail.com>
2024-10-04 19:33:17 +0530

    sdh

---
+++ ckuster.tf
@@ -0,0 +1,58 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+
+  cluster_name    =  "democluster"
+  cluster_version = "1.30"
+  subnet_ids  = module.vpc.private_subnets
+
+  cluster_endpoint_public_access  = true
+
+  cluster_addons = {
+    coredns                = {}
+    eks-pod-identity-agent = {}
+    kube-proxy             = {}
+    vpc-cni                = {}
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    example = {
+      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = ["t2.micro"]
+      vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+
+      min_size     = 2
+      max_size     = 4
+      desired_size = 2
+    }
+  }
+
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    # One access entry with a policy associated
+    example = {
+      kubernetes_groups = []
+      principal_arn     = "arn:aws:iam::058264111032:role/iamserviceaccount"
+
+      policy_associations = {
+        example = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSViewPolicy"
+          access_scope = {
+            namespaces = ["default"]
+            type       = "namespace"
+          }
+        }
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Terraform   = "true"
+  }
+}
